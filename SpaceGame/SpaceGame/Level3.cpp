908228783fb3f42af2b9bc04e302593d50b90ad5
JacoNeto <jaconeto8@gmail.com>
2022-09-19 20:45:21 -0300

    level 3

--- SpaceGame/SpaceGame/Level3.cpp
+++ SpaceGame/SpaceGame/Level3.cpp
@@ -19,9 +19,11 @@
 #include "Pivot.h"
 #include "Orb.h"
 #include "Cannon.h"
+#include "Bullet.h"
 #include "Wall.h"
 #include <string>
 #include <fstream>
+#include <ctime>
 #include "Level2.h"
 using std::ifstream;
 using std::string;
@@ -39,26 +41,25 @@ void Level3::Init()
     backg = new Sprite("Resources/phase_3_completed.png");
 
     // cria jogador
-    player = new Player(680.0f, 600.0f, RED);
+    player = new Player(680.0f, 690.0f, RED);
     player->currLevel = LEVEL3;
     scene->Add(player, MOVING);
     //temporario
     //player->currColor = RED;
 
     //Cria e posiciona bbox dos canhões
-    Cannon* cannon;
     //corner left cannon
-    cannon = new Cannon(-32, -27, 32, 27);
-    cannon->MoveTo(43, 26);
-    scene->Add(cannon, STATIC);
+    topLeftCannon = new Cannon(-32, -27, 32, 27);
+    topLeftCannon->MoveTo(43, 26);
+    scene->Add(topLeftCannon, STATIC);
     //bottom left cannon
-    cannon = new Cannon(-35, -23, 35, 23);
-    cannon->MoveTo(44, 464);
-    scene->Add(cannon, STATIC);
+    bottomLeftCannon = new Cannon(-35, -23, 35, 23);
+    bottomLeftCannon->MoveTo(44, 464);
+    scene->Add(bottomLeftCannon, STATIC);
     //right down cannon
-    cannon = new Cannon(-34.5, -27.5, 34.5, 27.5);
-    cannon->MoveTo(926, 399);
-    scene->Add(cannon, STATIC);
+    bottomRightCannon = new Cannon(-34.5, -27.5, 34.5, 27.5);
+    bottomRightCannon->MoveTo(926, 399);
+    scene->Add(bottomRightCannon, STATIC);
 
     // -----------------------------------------
     // posição das paredes
@@ -118,6 +119,37 @@ void Level3::Finalize()
 
 void Level3::Update()
 {
+
+    if ((clock() - start) % 233 == 0) {
+        Bullet* bl1 = new Bullet(300, 20);
+        bl1->MoveTo(bottomLeftCannon->X() + 20, bottomLeftCannon->Y() - 17, Layer::UPPER);
+        scene->Add(bl1, STATIC);
+
+        Bullet* bl2 = new Bullet(300, 66);
+        bl2->MoveTo(bottomLeftCannon->X() + 20, bottomLeftCannon->Y() + 17, Layer::UPPER);
+        scene->Add(bl2, STATIC);
+
+        Bullet* tr1 = new Bullet(-200, 100);
+        tr1->MoveTo(bottomRightCannon->X(), bottomRightCannon->Y() + 12, Layer::UPPER);
+        scene->Add(tr1, STATIC);
+
+        Bullet* tr2 = new Bullet(-250, 220);
+        tr2->MoveTo(bottomRightCannon->X(), bottomRightCannon->Y() - 15, Layer::UPPER);
+        scene->Add(tr2, STATIC);
+
+    }
+
+    if ((clock() - start) % 205 == 0) {
+
+        Bullet* br1 = new Bullet(300, 300);
+        br1->MoveTo(topLeftCannon->X() + 20, topLeftCannon->Y() + 7, Layer::UPPER);
+        scene->Add(br1, STATIC);
+
+        Bullet* br2 = new Bullet(300, 300);
+        br2->MoveTo(topLeftCannon->X() - 20, topLeftCannon->Y() - 7, Layer::UPPER);
+        scene->Add(br2, STATIC);
+
+    }
     
 
     // habilita/desabilita bounding box
